b1e0bc0f495b751a2a25036dfddeda3089bb50ba
Shubham Darbarsing Rajput <shubhamrajput252000@gmail.com>
2025-05-05 16:57:36 +0530

    feat: added playwright scrapper with script for render

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-# Install Chromium for Puppeteer
-apt-get update
-apt-get install -y chromium
-
-# Optional debug: confirm chromium path
-echo "Chromium installed at: $(which chromium)"
+npx playwright install --with-deps
