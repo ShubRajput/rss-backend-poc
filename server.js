2d74a2518a3b9342c15f2fba56261768634a2b2e
Shubham Darbarsing Rajput <shubhamrajput252000@gmail.com>
2025-05-21 21:37:19 +0530

    added port in env

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json()); // Parse incoming JSON requests
 app.use('/api', apiRoutes);
 
 // Start the server
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
